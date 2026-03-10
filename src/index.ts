4a1bb2eeefd0701fc5af1d07456ab6fcbffb8dd3
Catgirl <zeroaninea@gmail.com>
2026-03-10 13:52:36 +0400

    the first algorithm added

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,39 @@
 The current algorithm moves all duplicate keys into arrays.
 */
 
-console.log('Hello, World!');
+import fs from 'fs';
+
+const raw = fs.readFileSync(
+  './dictionary/English-phonetic-transcription.json',
+  'utf8',
+);
+
+const regex = /"([^"]+)"\s*:\s*"([^"]+)"/g;
+
+const dict: Record<string, { phonetics: string[]; correctCase?: string }> = {};
+
+let match;
+
+while ((match = regex.exec(raw)) !== null) {
+  const originalKey = match[1];
+  const value = match[2];
+
+  const lower = originalKey?.toLowerCase();
+
+  if (!dict[lower!]) {
+    dict[lower!] = {
+      phonetics: [],
+      correctCase: originalKey,
+    } as {
+      phonetics: string[];
+      correctCase?: string;
+    };
+  }
+
+  dict[lower!]?.phonetics.push(value!);
+}
+
+fs.writeFileSync(
+  './dictionary/English-phonetic-transcription-fixed.json',
+  JSON.stringify(dict, null, 2),
+);
